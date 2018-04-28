6d9db085fa0306ef5ba0906a56221b27dcaf63a4
yjyszzr <1053195207@qq.com>
2018-04-28 18:57:53 +0800

    修改 Change-Id: I22ca68b152dc38664a3c8474b7ef475e1ae667b4 Signed-off-by: yjyszzr <1053195207@qq.com>

--- member-api/src/main/java/com/dl/member/param/DeviceParam.java
+++ member-api/src/main/java/com/dl/member/param/DeviceParam.java
@@ -7,27 +7,47 @@ import lombok.Data;
 
 @Data
 public class DeviceParam {
-	 @ApiModelProperty(value = "手机品牌")
-	 private String brand;
-	 
-	 @ApiModelProperty(value = "版本号，如1.0.0")
-	 @NotNull(message = "app版本号不能为空")
-	 private String version;
-	 
-	 @ApiModelProperty("平台：0-ios 1-android")
-	 @NotNull(message = "操作系统不能为空")
+	 @ApiModelProperty("平台：0-iphone 1-android 2-h5(小写)")
+	 @NotNull(message = "平台不能为空")
 	 private String plat;
+	 
+	 @ApiModelProperty("协议版本号，初始值为1")
+	 private String apiv;
+	
+	 @ApiModelProperty(value = "app版本号，三位数字描述，例如：1.0.0")
+	 @NotNull(message = "app版本号不能为空")
+	 private String appv;
+	 
+	 @ApiModelProperty(value = "app唯一标识符。android取包名，IOS取值main BandleID")
+	 private String appid;
+	 
+	 @ApiModelProperty(value = "设备网卡mac地址")
+	 private String mac;
 	 
 	 @ApiModelProperty(value = "宽")
 	 private String w;
 	 
-	 @ApiModelProperty(value = "build")
+	 @ApiModelProperty(value = "高")
+	 private String h;
+	 
+	 @ApiModelProperty(value = "系统固件版本")
+	 private String os;
+	 
+	 @ApiModelProperty(value = "设备具体型号信息，据此判断配件类型")
+	 private String mid;
+	 
+	 @ApiModelProperty(value = "app编译时间，精确到秒")
 	 private String build;
 	 
-	 @ApiModelProperty(value = "channel")
+	 @ApiModelProperty(value = "手机品牌")
+	 private String brand;
+	 
+	 @ApiModelProperty(value = "推广渠道，如(1000a)")
 	 private String channel;
 	 
-	 @ApiModelProperty(value = "token")
+	 @ApiModelProperty(value = "前网络连接类型:WIFI、3G、2G、mobile和UNKNOWN")
+	 private String net;
+	 
+	 @ApiModelProperty(value = "用户账号验证串,用于验证用户id真实性")
 	 private String token;
-
 }
