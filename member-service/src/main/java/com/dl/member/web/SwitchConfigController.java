54c5d21a343e60633185b17941f18a9e6c2575eb
yjyszzr <1053195207@qq.com>
2018-05-02 18:12:02 +0800

    修改
    Change-Id: I9434e3dc8e01dae5fe6366fb2de9ed14cb0c748a
    Signed-off-by: yjyszzr <1053195207@qq.com>

--- member-service/src/main/java/com/dl/member/web/SwitchConfigController.java
+++ member-service/src/main/java/com/dl/member/web/SwitchConfigController.java
@@ -2,6 +2,7 @@ package com.dl.member.web;
 import com.alibaba.fastjson.JSON;
 import com.dl.base.model.UserDeviceInfo;
 import com.dl.base.result.BaseResult;
+import com.dl.base.result.ResultGenerator;
 import com.dl.base.util.DateUtil;
 import com.dl.base.util.SessionUtil;
 import com.dl.member.model.SwitchConfig;
@@ -36,7 +37,18 @@ public class SwitchConfigController {
     	UserDeviceInfo userDevice = SessionUtil.getUserDevice();
     	String inPrams = JSON.toJSONString(userDevice);
     	log.info(DateUtil.getCurrentDateTime()+"====================================版本参数:"+inPrams);
-    	return switchConfigService.querySwitchConfig(userDevice.getPlat(), userDevice.getAppv());
+    	String plat = "";
+    	if(userDevice.getPlat().equals("android")) {
+    		plat = "1";
+    	}else if(userDevice.getPlat().equals("iphone")) {
+    		plat = "0";
+    	}else if(userDevice.getPlat().equals("h5")) {
+    		plat = "2";
+    	}else {
+    		return ResultGenerator.genFailResult("设备信息中的plat参数错误");
+    	}
+    	
+    	return switchConfigService.querySwitchConfig(plat, userDevice.getAppv());
     }
 
 }
