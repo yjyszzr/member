e8595123641352a3812f0b7bb1ab51f389c627ae
123 <123@qq.com>
2018-05-02 17:19:25 +0800

    服务器验证码

---
+++ member-api/src/main/java/com/dl/member/param/SmsParamService.java
@@ -0,0 +1,21 @@
+package com.dl.member.param;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class SmsParamService {
+	 @ApiModelProperty(value = "手机号")
+	 @NotBlank(message = "手机号")
+	 private String mobile;
+
+	 @ApiModelProperty(value = "短信类型:0-短信登录验证码 1-注册验证码  2-忘记密码验证码")
+	 @NotBlank(message = "短信类型不能为空")
+	 private String smsType;
+
+	 @ApiModelProperty(value = "验证码")
+	 @NotBlank(message = "验证码")
+	 private String verifyCode;
+}
