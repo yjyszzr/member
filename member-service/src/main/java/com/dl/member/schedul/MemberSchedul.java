ca60560bae24571316c5d30ba0be63f42aa12d2b
duanyu <duanyu@caixiaomi.com>
2018-06-13 16:58:52 +0800

    启动定时任务

--- member-service/src/main/java/com/dl/member/schedul/MemberSchedul.java
+++ member-service/src/main/java/com/dl/member/schedul/MemberSchedul.java
@@ -4,18 +4,20 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
-import lombok.extern.slf4j.Slf4j;
-
 import org.apache.commons.collections.CollectionUtils;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import com.dl.base.util.DateUtil;
 import com.dl.member.dao.UserBonusMapper;
 import com.dl.member.service.UserBonusService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
-// @Configuration
-// @EnableScheduling
+@Configuration
+@EnableScheduling
 public class MemberSchedul {
 
 	@Resource
