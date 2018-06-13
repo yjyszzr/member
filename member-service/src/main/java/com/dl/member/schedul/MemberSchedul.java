c810c92819ec9d5110fba97d70f6287180a21bcd
yjyszzr <1053195207@qq.com>
2018-06-13 12:19:48 +0800

    modified
    Change-Id: I7cbe4a9fe3967dbf6df1b28031ab825aadcfba88
    Signed-off-by: yjyszzr <1053195207@qq.com>

--- member-service/src/main/java/com/dl/member/schedul/MemberSchedul.java
+++ member-service/src/main/java/com/dl/member/schedul/MemberSchedul.java
@@ -7,6 +7,8 @@ import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import com.dl.base.util.DateUtil;
@@ -14,8 +16,8 @@ import com.dl.member.dao.UserBonusMapper;
 import com.dl.member.service.UserBonusService;
 
 @Slf4j
-// @Configuration
-// @EnableScheduling
+@Configuration
+@EnableScheduling
 public class MemberSchedul {
 
 	@Resource
