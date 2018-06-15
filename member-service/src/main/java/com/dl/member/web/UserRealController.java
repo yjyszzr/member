0c1887d2fa6beb9c0265af4c2049b0295a7b3578
123 <123@qq.com>
2018-06-15 10:44:23 +0800

    18岁实名认证相关...

--- member-service/src/main/java/com/dl/member/web/UserRealController.java
+++ member-service/src/main/java/com/dl/member/web/UserRealController.java
@@ -1,4 +1,5 @@
 package com.dl.member.web;
+import com.alibaba.druid.util.StringUtils;
 import com.dl.base.result.BaseResult;
 import com.dl.base.result.ResultGenerator;
 import com.dl.member.dto.UserRealDTO;
@@ -6,10 +7,14 @@ import com.dl.member.param.RealNameAuthParam;
 import com.dl.member.param.StrParam;
 import com.dl.member.service.UserRealService;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.annotation.Resource;
 
 /**
@@ -18,6 +23,8 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("/user/real")
 public class UserRealController {
+	private final static Logger logger = LoggerFactory.getLogger(UserRealController.class);
+	
     @Resource
     private UserRealService userRealService;
 
@@ -29,8 +36,35 @@ public class UserRealController {
     @ApiOperation(value = "实名认证", notes = "实名认证")
     @PostMapping("/realNameAuth")
     public BaseResult<String> realNameAuth(@RequestBody RealNameAuthParam realNameAuthParam){
+    	String idCardNo = realNameAuthParam.getIDCode();
+    	if(StringUtils.isEmpty(idCardNo)) {
+    		return ResultGenerator.genResult(-1, "身份证号不能为空");
+    	}
+    	if(idCardNo.length() < 15) {
+    		return ResultGenerator.genResult(-1, "身份证号不合法");
+    	}
+    	int age = IdNOToAge(idCardNo);
+    	logger.info("[realNameAuth]" + " age:" + age);
     	return userRealService.realNameAuth(realNameAuthParam.getRealName(), realNameAuthParam.getIDCode());
     }
+    
+  //根据身份证号输出年龄
+   public int IdNOToAge(String IdNO){
+    int leh = IdNO.length();
+    String dates="";
+    if (leh == 18) {
+    	dates = IdNO.substring(6, 10);
+    	SimpleDateFormat df = new SimpleDateFormat("yyyy");
+        String year=df.format(new Date());
+        logger.info("[IdNOToAge]" +"");
+        int u=Integer.parseInt(year)-Integer.parseInt(dates);
+        return u;
+    }else{
+        dates = IdNO.substring(6, 8);
+        return Integer.parseInt(dates);
+    }
+
+}
     
     /**
      * 查询实名认证信息
