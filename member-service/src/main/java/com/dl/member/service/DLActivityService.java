f2471f354cf853472bd571088f90e30f8c5e3443
yjyszzr <1053195207@qq.com>
2018-07-24 15:18:20 +0800

    modified Change-Id: I78efdb7876881797b257ff0134aee8ccdfd496c7 Signed-off-by: yjyszzr <1053195207@qq.com>

--- member-service/src/main/java/com/dl/member/service/DLActivityService.java
+++ member-service/src/main/java/com/dl/member/service/DLActivityService.java
@@ -1,11 +1,18 @@
 package com.dl.member.service;
 import com.dl.member.model.DLActivity;
+
+import tk.mybatis.mapper.entity.Condition;
+
 import com.dl.member.dao.DLActivityMapper;
+import com.dl.member.dto.ActivityDTO;
 import com.dl.base.service.AbstractService;
 import com.dl.base.util.DateUtil;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.Resource;
 
@@ -29,4 +36,31 @@ public class DLActivityService extends AbstractService<DLActivity> {
     	Integer count = dLActivityMapper.countRechargeActivity(now);
     	return count;
     }
+    
+    /**
+     * 根据活动类型来查询活动集合
+     * @param actTypeParam
+     * @return
+     */
+    public List<ActivityDTO> queryActivityByActType(Integer actType){
+		ActivityDTO dlActivityDTO = new ActivityDTO();
+		Condition condition = new Condition(DLActivity.class);
+		condition.createCriteria().andCondition("act_type =",actType).andCondition("is_finish=", 0).andCondition("status=", 1).andGreaterThan("endTime", DateUtil.getCurrentTimeLong()).andLessThanOrEqualTo("startTime", DateUtil.getCurrentTimeLong());
+		List<DLActivity> lotteryActivitys = dLActivityMapper.selectByCondition(condition);
+		
+		List<ActivityDTO> lotteryActivityDTOs = new ArrayList<>();
+		if(lotteryActivitys.size() >= 0) {
+			lotteryActivitys.forEach(s->{
+				ActivityDTO activityDTO = new ActivityDTO();
+				activityDTO.setTitle(s.getActTitle());
+				activityDTO.setDetail("");
+				activityDTO.setIcon(s.getActImg());
+				activityDTO.setActUrl(s.getActUrl());
+				activityDTO.setIsFinish(s.getIsFinish());
+				lotteryActivityDTOs.add(activityDTO);
+			});
+		}
+		
+		return lotteryActivityDTOs;
+	}
 }
