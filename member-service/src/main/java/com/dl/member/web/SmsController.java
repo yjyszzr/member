1d39f30ae62b2853179a8dad77e720c5ecaf21d3
yjyszzr <1053195207@qq.com>
2018-08-06 10:12:09 +0800

    修改聚合短信频率限制 Change-Id: I9eeafe7b2c8ce6810c370ab11302f6e42327efc2 Signed-off-by: yjyszzr <1053195207@qq.com>

--- member-service/src/main/java/com/dl/member/web/SmsController.java
+++ member-service/src/main/java/com/dl/member/web/SmsController.java
@@ -78,12 +78,12 @@ public class SmsController {
 		}
 		
 		Long expireTimeLimit5 = stringRedisTemplate.getExpire(sendNumKey+"_3");
-		if(num == 3 && expireTimeLimit5 < 300 && expireTimeLimit5 > 0) {//聚合规定：5min 内不能超过3条
+		if(num == 3 && expireTimeLimit5 < 600 && expireTimeLimit5 > 0) {//聚合规定：10min 内不能超过3条
 			return ResultGenerator.genResult(MemberEnums.MESSAGE_5MIN_COUNT_ERROR.getcode(), MemberEnums.MESSAGE_5MIN_COUNT_ERROR.getMsg());
 		}
 		
 		Long expireTimeLimit60 = stringRedisTemplate.getExpire(sendNumKey+"_5");
-		if(num == 5 && expireTimeLimit60 < 3600 && expireTimeLimit60 > 0) {//聚合规定：60min 内不能超过5条
+		if(num == 4 && expireTimeLimit60 < 3600 && expireTimeLimit60 > 0) {//聚合规定：60min 内不能超过4条
 			return ResultGenerator.genResult(MemberEnums.MESSAGE_60MIN_COUNT_ERROR.getcode(), MemberEnums.MESSAGE_60MIN_COUNT_ERROR.getMsg());
 		}
 		
@@ -134,8 +134,8 @@ public class SmsController {
 		stringRedisTemplate.opsForValue().set(key, strRandom4, defineExpiredTime, TimeUnit.SECONDS);
 		int sendNumExpire = this.todayEndTime();
 		stringRedisTemplate.opsForValue().set(sendNumKey, num+"", sendNumExpire, TimeUnit.SECONDS);
-		stringRedisTemplate.opsForValue().set(sendNumKey+"_3", num+"", 300, TimeUnit.SECONDS);
-		stringRedisTemplate.opsForValue().set(sendNumKey+"_5", num+"", 3600, TimeUnit.SECONDS);
+		stringRedisTemplate.opsForValue().set(sendNumKey+"_3", num+"", 600, TimeUnit.SECONDS);
+		stringRedisTemplate.opsForValue().set(sendNumKey+"_4", num+"", 3600, TimeUnit.SECONDS);
 
 		return ResultGenerator.genSuccessResult("发送短信验证码成功");
 	}
