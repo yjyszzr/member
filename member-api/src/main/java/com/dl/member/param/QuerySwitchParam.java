82560a89a97237a5b72105dac50c42ec718618b8
duanyu <duanyu@caixiaomi.com>
2018-08-23 12:32:14 +0800

    update

--- member-api/src/main/java/com/dl/member/param/QuerySwitchParam.java
+++ member-api/src/main/java/com/dl/member/param/QuerySwitchParam.java
@@ -1,5 +1,19 @@
 package com.dl.member.param;
 
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
 public class QuerySwitchParam {
 
+	@ApiModelProperty(value = "空字符串")
+    private String str;
+    @ApiModelProperty(value = "经度")
+    private String longValue;
+    @ApiModelProperty(value = "纬度")
+    private String latValue;
+    @ApiModelProperty(value = "省份")
+    private String provinceCode;
+    @ApiModelProperty(value = "城市")
+    private String cityCode;
 }
