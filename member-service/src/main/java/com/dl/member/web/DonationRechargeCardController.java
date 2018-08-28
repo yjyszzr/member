f2fe9dd1dd15cc7a26fa702e169d7d62f15a172a
yjyszzr <1053195207@qq.com>
2018-08-28 17:54:46 +0800

    modified
    Change-Id: I502c80277eadc5ed196128b137b979521c5d69cc
    Signed-off-by: yjyszzr <1053195207@qq.com>

--- member-service/src/main/java/com/dl/member/web/DonationRechargeCardController.java
+++ member-service/src/main/java/com/dl/member/web/DonationRechargeCardController.java
@@ -1,26 +1,15 @@
 package com.dl.member.web;
-import com.dl.base.result.BaseResult;
-import com.dl.base.result.ResultGenerator;
-import com.dl.member.dto.DonationRechargeCardDTO;
-import com.dl.member.dto.RechargeActivityDTO;
-import com.dl.member.dto.UserBonusDTO;
-import com.dl.member.model.DonationRechargeCard;
-import com.dl.member.param.PageParam;
-import com.dl.member.service.DonationRechargeCardService;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
+import javax.annotation.Resource;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.dl.base.result.BaseResult;
+import com.dl.member.dto.RechargeActivityDTO;
+import com.dl.member.param.PageParam;
+import com.dl.member.service.DonationRechargeCardService;
 
 /**
 * Created by CodeGenerator on 2018/05/30.
