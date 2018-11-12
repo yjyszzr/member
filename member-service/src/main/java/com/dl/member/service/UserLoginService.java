dcd7ef248cce47351fcdd0b79db85a7cc82b9829
wenhuaiteng <wenhuaiteng@126.com>
2018-11-12 12:24:05 +0800

    登录日志...

--- member-service/src/main/java/com/dl/member/service/UserLoginService.java
+++ member-service/src/main/java/com/dl/member/service/UserLoginService.java
@@ -99,7 +99,7 @@ public class UserLoginService extends AbstractService<UserLoginLog> {
 				return ResultGenerator.genResult(userLoginRst.getCode(), userLoginRst.getMsg());
 			}
 			userLoginDTO = userLoginRst.getData();
-
+			logger.info("[loginByPass]" + " mobile:" + mobile + " userLoginDTO:" + userLoginDTO);
 			if (!userLoginMobileParam.getLoginSource().equals(ProjectConstant.LOGIN_SOURCE_H5)) {
 				if (null == userLoginMobileParam.getPushKey()) {
 					this.updatePushKey("", user);
