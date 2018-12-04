984c054ffbb3024f29ccc67d2dac4bff17b60fc2
zzr <zhangzirong@caixiaomi.com>
2018-12-04 19:02:41 +0800

    modified

--- member-service/src/main/java/com/dl/member/web/SysConfigController.java
+++ member-service/src/main/java/com/dl/member/web/SysConfigController.java
@@ -1,6 +1,7 @@
 package com.dl.member.web;
 import com.dl.base.result.BaseResult;
 import com.dl.base.result.ResultGenerator;
+import com.dl.member.dto.ServiceSwitchDTO;
 import com.dl.member.dto.SysConfigDTO;
 import com.dl.member.model.SwitchConfig;
 import com.dl.member.model.SysConfig;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -36,5 +38,15 @@ public class SysConfigController {
     public BaseResult<SysConfigDTO> querySysConfig(@RequestBody SysConfigParam sysConfigParam) {
     	SysConfigDTO sysConfigDTO = sysConfigService.querySysConfig(sysConfigParam.getBusinessId());
     	return ResultGenerator.genSuccessResult("success", sysConfigDTO);
+    }
+
+    @ApiOperation(value = "根据业务id查询某个功能是否开启", notes = "根据业务id查询某个功能是否开启i")
+    @PostMapping("/queryFuncByBusiId")
+    public BaseResult<ServiceSwitchDTO> queryFuncByBusiId(@Valid @RequestBody SysConfigParam sysConfigParam) {
+        SysConfigDTO sysConfigDTO = sysConfigService.querySysConfig(sysConfigParam.getBusinessId());
+        ServiceSwitchDTO serviceSwitchDTO = new ServiceSwitchDTO();
+        serviceSwitchDTO.setBusinessId(sysConfigDTO.getBusinessId());
+        serviceSwitchDTO.setTurnOn(sysConfigDTO.getValue().compareTo(BigDecimal.ZERO) == 0 ?"0":"1");
+        return ResultGenerator.genSuccessResult("success", serviceSwitchDTO);
     }
 }
