c49ec8bde43ab34a5f93803bc77ef108ad9e094b
zzr <zhangzirong@caixiaomi.com>
2018-12-10 18:52:22 +0800

    modified

--- member-service/src/main/java/com/dl/member/service/UserLoginService.java
+++ member-service/src/main/java/com/dl/member/service/UserLoginService.java
@@ -441,8 +441,8 @@ public class UserLoginService extends AbstractService<UserLoginLog> {
 		ull.setImei("");
 		ull.setLoginSource("");
 		ull.setDeviceChannel(device.getChannel());
-		ull.setLon(device.getLon() != null?0:device.getLon());
-		ull.setLat(device.getLat() != null?0:device.getLat());
+		ull.setLon(device.getLon() != null?device.getLon():0);
+		ull.setLat(device.getLat() != null?device.getLat():0);
 		log.info(JSON.toJSONString(device));
 		if(!StringUtils.isEmpty(device.getCity())  && !StringUtils.isEmpty(device.getProvince())){
 			try {
