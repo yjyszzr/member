a91f8f6b4d938eb49c8d9bf4377e7a4ef21ac3be
zzr <zhangzirong@caixiaomi.com>
2018-12-27 16:11:57 +0800

    modified

--- member-service/src/main/java/com/dl/member/dao/SysConfigMapper.java
+++ member-service/src/main/java/com/dl/member/dao/SysConfigMapper.java
@@ -2,11 +2,11 @@ package com.dl.member.dao;
 
 import com.dl.base.mapper.Mapper;
 import com.dl.member.model.SysConfig;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface SysConfigMapper extends Mapper<SysConfig> {
-
-    public List<SysConfig> queryBusiIds(@Param("queryBusiIds") List<Integer> queryBusiIds);
+    public List<SysConfig> queryBusiIds(@Param("businessIds") List<Integer> businessIds);
 
 }
