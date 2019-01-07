5c56745e851306da12762df1f635075297a97e80
zzr <zhangzirong@caixiaomi.com>
2019-01-07 13:46:12 +0800

    modified

--- member-api/src/main/java/com/dl/member/param/BusiIdsListParam.java
+++ member-api/src/main/java/com/dl/member/param/BusiIdsListParam.java
@@ -4,7 +4,6 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @ApiModel("业务配置id集合信息")
@@ -12,7 +11,6 @@ import java.util.List;
 public class BusiIdsListParam {
 
     @ApiModelProperty("业务id集合")
-    @NotNull
     private List<Integer> businessIdList;
 
 }
