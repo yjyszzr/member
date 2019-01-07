de1d544c78513c7537c2bbe7c601d0ad79a7b100
zzr <zhangzirong@caixiaomi.com>
2019-01-07 13:48:51 +0800

    modified

--- member-api/src/main/java/com/dl/member/param/BonusLimitConditionParam.java
+++ member-api/src/main/java/com/dl/member/param/BonusLimitConditionParam.java
@@ -1,19 +1,16 @@
 package com.dl.member.param;
 
-import java.math.BigDecimal;
-
-import javax.validation.constraints.NotNull;
-
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
+import java.math.BigDecimal;
 
 @ApiModel("订单实际付款金额")
 @Data
 public class BonusLimitConditionParam {
 
 	@ApiModelProperty("订单实际付款金额")
-	@NotNull
     private BigDecimal orderMoneyPaid;
 	
     //可定义其他参数
--- member-service/src/main/java/com/dl/member/web/UserBonusController.java
+++ member-service/src/main/java/com/dl/member/web/UserBonusController.java
@@ -3,10 +3,12 @@ package com.dl.member.web;
 import com.dl.base.param.EmptyParam;
 import com.dl.base.result.BaseResult;
 import com.dl.base.result.ResultGenerator;
-import com.dl.base.util.SessionUtil;
 import com.dl.member.dto.DonationPriceDTO;
 import com.dl.member.dto.UserBonusDTO;
-import com.dl.member.param.*;
+import com.dl.member.param.BonusLimitConditionParam;
+import com.dl.member.param.PayLogIdParam;
+import com.dl.member.param.UserBonusIdParam;
+import com.dl.member.param.UserBonusStatusParam;
 import com.dl.member.service.UserBonusService;
 import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.ApiOperation;
@@ -49,23 +51,6 @@ public class UserBonusController {
     	return ResultGenerator.genSuccessResult("查询用户有效的红包列表成功",userBonusDTOList);
     }
 
-    @ApiOperation(value="根据状态查询有效的红包集合V2", notes="根据状态查询有效的红包集合V2",hidden=false)
-    @PostMapping("/queryBonusListByStatus")
-    public BaseResult<PageInfo<UserBonusDTO>> queryBonusListByStatusV2(@RequestBody UserBonusStatusParam userBonusStatusParam) {
-        PageInfo<UserBonusDTO> userBonusDTOList =  userBonusService.queryBonusListByStatus(userBonusStatusParam.getStatus(),userBonusStatusParam.getPageNum(),userBonusStatusParam.getPageSize());
-        return ResultGenerator.genSuccessResult("根据状态查询有效的红包集合成功",userBonusDTOList);
-    }
-
-    @ApiOperation(value="支付的时候查询用户可用的红包列表V2，前端直接调用", notes="支付的时候查询用户可用的红包列表V2：前端直接调用",hidden=false)
-    @PostMapping("/queryValidBonusListV2")
-    public BaseResult<List<UserBonusDTO>> queryValidBonusListV2(@RequestBody OrderSnParam orderSnParam) {
-        Integer userId = SessionUtil.getUserId();
-        if (null == userId) {
-            return ResultGenerator.genNeedLoginResult("请登录");
-        }
-        List<UserBonusDTO> userBonusDTOList =  userBonusService.queryValidBonusListForPayV2(orderSnParam,userId);
-        return ResultGenerator.genSuccessResult("查询用户有效的红包列表成功",userBonusDTOList);
-    }
     
     //20180831 由于充值成功的时间是有些缓慢的，前端并不知道该显示多少钱的奖杯，所以去掉
     @ApiOperation(value="领取充值后 弹出的赠送金额", notes="领取充值后 弹出的赠送金额",hidden=false)
