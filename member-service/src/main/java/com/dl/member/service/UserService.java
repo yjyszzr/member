0bbce89c6bd81edc5de371d3aa5a605736d48b68
lifeilong <lifeilong@caixiaomi.com>
2019-05-10 09:50:20 +0800

    修改

--- member-service/src/main/java/com/dl/member/service/UserService.java
+++ member-service/src/main/java/com/dl/member/service/UserService.java
@@ -26,6 +26,7 @@ import com.dl.member.util.Encryption;
 import com.dl.member.util.TokenUtil;
 import com.dl.shop.auth.api.IAuthService;
 import com.dl.shop.payment.api.IpaymentService;
+import com.dl.shop.payment.dto.PayLogDTO;
 import com.dl.shop.payment.dto.UserRechargeDTO;
 import com.dl.shop.payment.param.StrParam;
 
@@ -223,9 +224,9 @@ public class UserService extends AbstractService<User> {
 			userDTO.setWithdrawTurnOn(String.valueOf(sDto3.getValue().intValue()));
 		}
 		if(userId==1000000077) {//财务账号--财务账号提现金额为商户余额
-			StrParam strParam = new StrParam();
-			BaseResult<UserRechargeDTO> se = paymentService.queryUserRechargeListByUserId(strParam);
-			BaseResult<Map<String,Object>> ymoney = (BaseResult<Map<String,Object>>) paymentService.getShMoney();
+			com.dl.shop.payment.param.PayLogIdParam strParam = new com.dl.shop.payment.param.PayLogIdParam();
+			BaseResult<PayLogDTO> se = paymentService.queryPayLogByPayLogId(strParam);
+//			BaseResult<Map<String,Object>> ymoney = (BaseResult<Map<String,Object>>) paymentService.getShMoney();
 //			if(ymoney!=null && ymoney.getData()!=null) {
 //				userDTO.setUserMoney(ymoney.getData().get("account_balance").toString());//账户余额
 //				userDTO.setBalance("0");
