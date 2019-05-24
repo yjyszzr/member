73a82687ad0f28933eeb19c29108e7deb5d183e9
zhangzirong <zhangzirong@caixiaomi.com>
2019-05-24 11:57:09 +0800

    Update SwitchConfigService.java

--- member-service/src/main/java/com/dl/member/service/SwitchConfigService.java
+++ member-service/src/main/java/com/dl/member/service/SwitchConfigService.java
@@ -20,7 +20,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import com.dl.member.dto.SysConfigDTO;
 import javax.annotation.Resource;
 import java.util.List;
 
